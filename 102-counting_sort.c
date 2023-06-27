294b55ea2d065c5fbaec91d44e58fec1aadd4d84
Ellahjonnez <ellahjonnez@gmail.com>
2023-06-27 09:50:36 -0700

    Task6: Counting sort

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,14 +1,10 @@
 #include "sort.h"
-
 /**
- * counting_sort - To sort an array of integers in
- * ascending order using the Counting sort algorithm
+ * counting_sort - To sort an array of integers in ascending order
  * @array: array
  * @size: size
- *
  * Return: no return
  */
-
 void counting_sort(int *array, size_t size)
 {
 	int n;
@@ -17,25 +13,20 @@ void counting_sort(int *array, size_t size)
 
 	if (size < 2)
 		return;
-
 	for (n = i = 0; i < (int)size; i++)
 		if (array[i] > n)
 			n = array[i];
-
 	buff = malloc(sizeof(int) * (n + 1));
 	if (!buff)
 		return;
-
 	for (i = 0; i <= n; i++)
 		buff[i] = 0;
 	for (i = 0; i < (int)size; i++)
 		buff[array[i]] += 1;
 	for (i = 1; i <= n; i++)
 		buff[i] += buff[i - 1];
-
 	print_array(buff, (n + 1));
 	aux1 = malloc(sizeof(int) * (size + 1));
-
 	if (!aux1)
 	{
 		free(buff);
@@ -46,10 +37,8 @@ void counting_sort(int *array, size_t size)
 		aux1[buff[array[i]] - 1] = array[i];
 		buff[array[i]] -= 1;
 	}
-
 	for (i = 0; i < (int)size; i++)
 		array[i] = aux1[i];
-
 	free(buff);
 	free(aux1);
 }
